6b3972191e5d134f6db76538224720280d6f9936
root <aaa>
2019-11-06 02:13:25 -0500

    fix install script

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-apt install libsnmp-dev snmp-mibs-downloader gcc python3-dev python-dev
-pip3 install -r requirements
+apt install libsnmp-dev snmp-mibs-downloader gcc python3-dev python-dev &&
+pip3 install -r requirements.txt &&
 
 rm -rf /usr/bin/async-connect-scan 2>/dev/null
 rm -rf /usr/bin/async-snmp-scan 2>/dev/null
 rm -rf /usr/bin/async-web-scan 2>/dev/null
 
-link async-connect-scan.py /usr/bin/async-connect-scan
-link async-snmp-scan.py /usr/bin/async-snmp-scan
-link async-web-scan.py /usr/bin/async-web-scan
+link async-connect-scan.py /usr/bin/async-connect-scan &&
+link async-snmp-scan.py /usr/bin/async-snmp-scan &&
+link async-web-scan.py /usr/bin/async-web-scan &&
+echo 'OK.'
