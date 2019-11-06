1f50ef938788ae766412dfe75a9e7f82283d8805
root <aaa>
2019-11-06 07:39:26 -0500

    fix install script

--- install.sh
+++ install.sh
@@ -6,8 +6,10 @@ pip3 install -r requirements.txt &&
 rm -rf /usr/bin/async-connect-scan 2>/dev/null
 rm -rf /usr/bin/async-snmp-scan 2>/dev/null
 rm -rf /usr/bin/async-web-scan 2>/dev/null
+rm -rf /usr/bin/async-web-fuzzer 2>/dev/null
 
 link async-connect-scan.py /usr/bin/async-connect-scan &&
 link async-snmp-scan.py /usr/bin/async-snmp-scan &&
 link async-web-scan.py /usr/bin/async-web-scan &&
+link async-web-fuzzer.py /usr/bin/async-web-fuzzer &&
 echo 'OK.'
