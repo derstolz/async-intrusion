03e90b16b974794e46ac7f15b49085363732ad1c
Daniel <30957352+derstolz@users.noreply.github.com>
2019-11-13 22:00:17 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,12 @@ rm -rf /usr/bin/async-connect-scan 2>/dev/null
 rm -rf /usr/bin/async-snmp-scan 2>/dev/null
 rm -rf /usr/bin/async-web-scan 2>/dev/null
 rm -rf /usr/bin/async-web-fuzzer 2>/dev/null
+rm -rf /usr/bin/async-robots-scan 2>/dev/null
+
 
 link async-connect-scan.py /usr/bin/async-connect-scan &&
 link async-snmp-scan.py /usr/bin/async-snmp-scan &&
 link async-web-scan.py /usr/bin/async-web-scan &&
 link async-web-fuzzer.py /usr/bin/async-web-fuzzer &&
+link async-robots-scan.py /usr/bin/async-robots-scan &&
 echo 'OK.'
